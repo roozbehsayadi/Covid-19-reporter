8bef31f23f6177dc2c19d85e7e517b07ab313e54
Roozbeh <roozbehsayadi@gmail.com>
2020-03-28 19:37:53 +0430

    Added environment variables.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+
+const port = process.env.PORT;
+
 const router = require('./controller/controller')
 
 app.use('/gis', router);
