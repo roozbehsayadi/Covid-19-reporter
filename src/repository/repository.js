1b8b73ee305e97934f2afa6521646ab259e639b9
Roozbeh <roozbehsayadi@gmail.com>
2020-03-29 02:43:35 +0430

    Added some functions for getting and editing polygons.

--- src/repository/repository.js
+++ src/repository/repository.js
@@ -1,3 +1,16 @@
 const fs = require("fs");
 
 let polygonsGISData = JSON.parse(fs.readFileSync('resources/repository.json'));
+
+const getPolygons = () => {
+    return polygonsGISData.features;
+};
+
+const addPolygon = (polygon) => {
+    polygonsGISData.features.push(polyton);
+};
+
+module.exports = {
+    getPolygons: getPolygons,
+    addPolygon: addPolygon
+};
