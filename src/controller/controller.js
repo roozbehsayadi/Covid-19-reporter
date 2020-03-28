63ba21729c4d9e737af4facacd598884aba47f26
Roozbeh <roozbehsayadi@gmail.com>
2020-03-28 19:02:16 +0430

    Implemented routing frames.

---
+++ src/controller/controller.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/testpoint', function(req, res) {
+    res.send('in testpoint function');
+})
+
+router.put('/addpolygon', function(req, res){
+    res.send('in addpolygon functoin');
+})
+
+module.exports = router;
+
