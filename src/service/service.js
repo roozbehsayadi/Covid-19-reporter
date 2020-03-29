2d231d8760dd8202a29159058cfd1d52a680e545
Roozbeh <roozbehsayadi@gmail.com>
2020-03-29 13:23:05 +0430

    Converted spaces to tabs.

--- src/service/service.js
+++ src/service/service.js
@@ -2,27 +2,27 @@ var repository = require('./../repository/repository');
 var inside = require('point-in-polygon');
 
 const checkIfPointInsidePolygon = (point, polygon) => {
-    if (inside(point, polygon))
-        return polygon.properties.name;
+	if (inside(point, polygon))
+		return polygon.properties.name;
 };
 
 const getSurroundingPolygons = (point) => {
-    let returnValue = [];
-    repository.getPolygons().forEach(element => {
-        if ( element == null )
-            return;
-        if (checkIfPointInsidePolygon(point, element.geometry.coordinates[0])){
-            returnValue.push(element.properties.name);
-        }
-    });
-    return returnValue; 
+	let returnValue = [];
+	repository.getPolygons().forEach(element => {
+		if ( element == null )
+			return;
+		if (checkIfPointInsidePolygon(point, element.geometry.coordinates[0])){
+			returnValue.push(element.properties.name);
+		}
+	});
+	return returnValue; 
 };
 
 const addPolyogon = polygon => {
-    repository.addPolygon(polygon);
+	repository.addPolygon(polygon);
 };
 
 module.exports = {
-    addPolygon: this.addPolygon,
-    getSurroundingPolygons: getSurroundingPolygons
+	addPolygon: this.addPolygon,
+	getSurroundingPolygons: getSurroundingPolygons
 };
